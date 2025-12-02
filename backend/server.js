afffaca95cc9f83e6fe4ccc831133a2e3af52fcc
Adham alaa <adhamalaa8086@gmail.com>
2025-12-02 21:31:10 +0200

    vercel1.1

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://ecommerce-store-api-sand.vercel.app/",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true
 }));
