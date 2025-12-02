f255a6250502e10a7217b2c1d92e0b75908074ac
Adham alaa <adhamalaa8086@gmail.com>
2025-12-02 23:03:55 +0200

    123

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -25,13 +25,13 @@ const setCookies = (res, accessToken, refreshToken) => {
     res.cookie("accessToken", accessToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "Strict",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         maxAge: 15 * 60 * 1000,
     });
     res.cookie("refreshToken", refreshToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "Strict",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 };
