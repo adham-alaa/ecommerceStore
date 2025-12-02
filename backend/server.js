fa0e216566f43b2bf65522ec6063bca7060446f0
Adham alaa <adhamalaa8086@gmail.com>
2025-12-02 21:27:21 +0200

    vercel1

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true
+}));
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 
@@ -31,10 +35,8 @@ app.use("/api/analytics", analyticsRoutes)
 app.use("/api/categories", categoryRoutes);
 
 app.listen(PORT, () => {
-    console.log("server is running on http://localhost:" + PORT)
+    console.log("server is running");
 
     connectDB();
 
 });
-
-// G6cWvxJ36ElswL3a
